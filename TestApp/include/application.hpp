169c61bba70904cf90d1ef9d9e70993064231f65
HackFight <hackfightowo@gmail.com>
2026-08-03 00:28:15 +0200

    TODO: Shaders

--- TestApp/include/application.hpp
+++ TestApp/include/application.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <Volk/volk.h>
+#include <vector>
 
 class Application {
 	constexpr static uint32_t VulkanVersion{ VK_API_VERSION_1_4 };
+	constexpr static VkFormat swapchainFormat{ VK_FORMAT_B8G8R8A8_SRGB };
+	constexpr static VkFormat depthFormat{ VK_FORMAT_D32_SFLOAT };
 
 public:
 	bool initialize();
@@ -16,6 +19,10 @@ private:
 	bool createVulkanSurface();
 	VkPhysicalDevice findPhysicalDevice();
 	bool findGraphicsQueue();
+	bool createLogicalDevice(VkPhysicalDevice physicalDevice);
+	bool initializeVMA();
+	bool createSwapchain(uint32_t width, uint32_t height);
+	void destroySwapchain();
 
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -24,11 +31,26 @@ private:
 		void* pUserData);
 
 	SDL_Window* window = nullptr;
-	VkInstance vulkanInstance = nullptr;
-	VkSurfaceKHR surface = nullptr;
-	VkPhysicalDevice physicalDevice = nullptr;
-	uint32_t gfxQueueFamIdx = UINT32_MAX;
-	VkQueue gfxQueue = nullptr;
 	uint32_t width = 800;
 	uint32_t height = 600;
+
+	VkInstance vulkanInstance = nullptr;
+	VkPhysicalDevice physicalDevice = nullptr;
+	VkDevice device = nullptr;
+	VkSurfaceKHR surface = nullptr;
+	VmaAllocator vmaAllocator = nullptr;
+
+	uint32_t gfxQueueFamIdx = UINT32_MAX;
+	VkQueue gfxQueue = nullptr;
+
+	uint32_t swapchainWidth = 0;
+	uint32_t swapchainHeight = 0;
+	VkSwapchainKHR swapchain = nullptr;
+	std::vector<VkImage> swapchainImages;
+	std::vector<VkImageView> swapchainImageViews;
+	std::vector<VkSemaphore> renderCompleteSemaphores;
+
+	VkImage depthImage = nullptr;
+	VkImageView depthImageView = nullptr;
+	VmaAllocation depthImageAllocation = nullptr;
 };
--- TestApp/src/application.cpp
+++ TestApp/src/application.cpp
@@ -2,6 +2,9 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+
+#include <vma/vk_mem_alloc.h>
+
 #include <iostream>
 #include <vector>
 
@@ -42,6 +45,26 @@ void Application::run()
 
 void Application::shutdown()
 {
+	destroySwapchain();
+
+	if (vmaAllocator)
+	{
+		vmaDestroyAllocator(vmaAllocator);
+		vmaAllocator = nullptr;
+	}
+
+	if (surface)
+	{
+		vkDestroySurfaceKHR(vulkanInstance, surface, nullptr);
+		surface = nullptr;
+	}
+
+	if (device)
+	{
+		vkDestroyDevice(device, nullptr);
+		device = nullptr;
+	}
+
 	if (vulkanInstance)
 	{
 		vkDestroyInstance(vulkanInstance, nullptr);
@@ -83,7 +106,25 @@ bool Application::initializeVulkan()
 		return false;
 	}
 
-	//TODO create logical device
+	if (!createLogicalDevice(physicalDevice))
+	{
+		std::cerr << "Error creating logical device" << std::endl;
+		return false;
+	}
+
+	if(!initializeVMA())
+	{
+		std::cerr << "Error initializing Vulkan Memory Allocator" << std::endl;
+		return false;
+	}
+
+	if (!createSwapchain(width, height))
+	{
+		std::cerr << "Error creating swapchain" << std::endl;
+		return false;
+	}
+
+	// TODO Shader
 }
 
 bool Application::createVulkanInstance()
@@ -187,6 +228,27 @@ VkPhysicalDevice Application::findPhysicalDevice()
 			}
 		}
 	}
+
+	uint32_t formatCount = 0;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, nullptr);
+	std::vector<VkSurfaceFormatKHR> surfaceFormats(formatCount);
+	vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, surfaceFormats.data());
+
+	bool formatSupported = false;
+	for (const VkSurfaceFormatKHR& surfaceFormat : surfaceFormats)
+	{
+		if (surfaceFormat.format == swapchainFormat)
+		{
+			formatSupported = true;
+			break;
+		}
+	}
+	if (!formatSupported)
+	{
+		std::cerr << "Required swapchain format not supported by the surface" << std::endl;
+		return nullptr;
+	}
+
 	return physicalDevice;
 }
 
@@ -211,4 +273,261 @@ bool Application::findGraphicsQueue()
 		}
 	}
 	return false;
+}
+
+bool Application::createLogicalDevice(VkPhysicalDevice physicalDevice)
+{
+	VkPhysicalDeviceVulkan14Features supportedFeatures14
+	{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_4_FEATURES, .pNext = nullptr };
+	VkPhysicalDeviceVulkan13Features supportedFeatures13
+	{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES, .pNext = &supportedFeatures14 };
+	VkPhysicalDeviceVulkan12Features supportedFeatures12
+	{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES, .pNext = &supportedFeatures13 };
+	VkPhysicalDeviceFeatures2 supportedFeatures
+	{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2, .pNext = &supportedFeatures12 };
+	vkGetPhysicalDeviceFeatures2(physicalDevice, &supportedFeatures);
+
+	if (!supportedFeatures13.dynamicRendering || !supportedFeatures13.synchronization2 ||
+		!supportedFeatures12.timelineSemaphore)
+	{
+		std::cerr << "Required Vulkan features not supported by the physical device" << std::endl;
+		return false;
+	}
+
+	VkPhysicalDeviceVulkan14Features features14
+	{
+		.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_4_FEATURES,
+		.pNext = nullptr,
+	};
+	VkPhysicalDeviceVulkan13Features features13
+	{
+		.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES,
+		.pNext = &features14,
+		.synchronization2 = VK_TRUE,
+		.dynamicRendering = VK_TRUE,
+	};
+	VkPhysicalDeviceVulkan12Features features12
+	{
+		.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES,
+		.pNext = &features13,
+		.timelineSemaphore = VK_TRUE,
+	};
+	VkPhysicalDeviceFeatures2 features
+	{
+		.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2,
+		.pNext = &features12,
+	};
+
+	std::vector<float> queuePriorities{ 1.0f };
+	VkDeviceQueueCreateInfo gfxQueueInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+		.queueFamilyIndex = gfxQueueFamIdx,
+		.queueCount = 1,
+		.pQueuePriorities = queuePriorities.data(),
+	};
+
+	const std::vector<const char*> deviceExtensions{ VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+	VkDeviceCreateInfo deviceCreateInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+		.pNext = &features,
+		.queueCreateInfoCount = 1,
+		.pQueueCreateInfos = &gfxQueueInfo,
+		.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size()),
+		.ppEnabledExtensionNames = deviceExtensions.data(),
+		.pEnabledFeatures = nullptr,
+	};
+
+	if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS)
+	{
+		return false;
+	}
+
+	vkGetDeviceQueue(device, gfxQueueFamIdx, 0, &gfxQueue);
+	if (!gfxQueue)
+	{
+		std::cerr << "Failed to retrieve graphics queue" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool Application::initializeVMA()
+{
+	VmaVulkanFunctions vmaFuncInfo{};
+	VmaAllocatorCreateInfo vmaAllocInfo
+	{
+		.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
+		.physicalDevice = physicalDevice,
+		.device = device,
+		.pVulkanFunctions = &vmaFuncInfo,
+		.instance = vulkanInstance,
+		.vulkanApiVersion = VulkanVersion,
+	};
+
+	vmaImportVulkanFunctionsFromVolk(&vmaAllocInfo, &vmaFuncInfo);
+
+	if (vmaCreateAllocator(&vmaAllocInfo, &vmaAllocator) != VK_SUCCESS)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool Application::createSwapchain(uint32_t width, uint32_t height)
+{
+	swapchainWidth = width;
+	swapchainHeight = height;
+
+	VkSurfaceCapabilitiesKHR surfaceCapabilities{};
+	if (vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfaceCapabilities) != VK_SUCCESS)
+	{
+		std::cerr << "Failed to get surface capabilities" << std::endl;
+		return false;
+	}
+
+	uint32_t requestedImageCount = std::max(2u, surfaceCapabilities.minImageCount);
+	if (surfaceCapabilities.maxImageCount > 0)
+	{
+		requestedImageCount = std::min(requestedImageCount, surfaceCapabilities.maxImageCount);
+	}
+
+	VkSwapchainCreateInfoKHR swapchainCreateInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+		.surface = surface,
+		.minImageCount = requestedImageCount,
+		.imageFormat = swapchainFormat,
+		.imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
+		.imageExtent = { .width = swapchainWidth, .height = swapchainHeight },
+		.imageArrayLayers = 1,
+		.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+		.preTransform = surfaceCapabilities.currentTransform,
+		.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+		.presentMode = VK_PRESENT_MODE_FIFO_KHR,
+	};
+
+	if (vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapchain) != VK_SUCCESS)
+	{
+		std::cerr << "Failed to create swapchain" << std::endl;
+		return false;
+	}
+
+	uint32_t imageCount = 0;
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr);
+	swapchainImages.resize(imageCount);
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, swapchainImages.data());
+	swapchainImageViews.resize(imageCount);
+
+	for (size_t i = 0; i < swapchainImages.size(); ++i)
+	{
+		VkImageViewCreateInfo imgViewInfo
+		{
+			.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+			.image = swapchainImages[i],
+			.viewType = VK_IMAGE_VIEW_TYPE_2D,
+			.format = swapchainFormat,
+			.subresourceRange
+			{
+				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1,
+			}
+		};
+
+		if (vkCreateImageView(device, &imgViewInfo, nullptr, &swapchainImageViews[i]) != VK_SUCCESS)
+		{
+			std::cerr << "Failed to create swapchain image view " << i << std::endl;
+			return false;
+		}
+	}
+
+	renderCompleteSemaphores.resize(swapchainImages.size());
+	for (VkSemaphore& semaphore : renderCompleteSemaphores)
+	{
+		VkSemaphoreCreateInfo semaphoreInfo{ .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+		if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &semaphore) != VK_SUCCESS)
+		{
+			std::cerr << "Failed to create render-complete semaphore" << std::endl;
+			return false;
+		}
+	}
+
+	VkImageCreateInfo depthCreateInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+		.imageType = VK_IMAGE_TYPE_2D,
+		.format = depthFormat,
+		.extent{.width = swapchainWidth, .height = swapchainHeight, .depth = 1 },
+		.mipLevels = 1,
+		.arrayLayers = 1,
+		.samples = VK_SAMPLE_COUNT_1_BIT,
+		.tiling = VK_IMAGE_TILING_OPTIMAL,
+		.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+		.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+	};
+
+	VmaAllocationCreateInfo allocInfo
+	{
+		.flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT,
+		.usage = VMA_MEMORY_USAGE_AUTO,
+	};
+	if (vmaCreateImage(vmaAllocator, &depthCreateInfo, &allocInfo, &depthImage, &depthImageAllocation, nullptr) != VK_SUCCESS)
+	{
+		std::cerr << "Failed to create depth image" << std::endl;
+		return false;
+	}
+
+	VkImageViewCreateInfo depthImgViewInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+		.image = depthImage,
+		.viewType = VK_IMAGE_VIEW_TYPE_2D,
+		.format = depthFormat,
+		.subresourceRange
+		{
+			.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT,
+			.levelCount = 1,
+			.layerCount = 1,
+		}
+	};
+	if (vkCreateImageView(device, &depthImgViewInfo, nullptr, &depthImageView) != VK_SUCCESS)
+	{
+		std::cerr << "Failed to create depth image view" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void Application::destroySwapchain()
+{
+	for (VkImageView swapchainImageView : swapchainImageViews)
+	{
+		vkDestroyImageView(device, swapchainImageView, nullptr);
+	}
+	swapchainImageViews.clear();
+
+	for (VkSemaphore semaphore : renderCompleteSemaphores)
+	{
+		vkDestroySemaphore(device, semaphore, nullptr);
+	}
+	renderCompleteSemaphores.clear();
+
+	if (swapchain)
+	{
+		vkDestroySwapchainKHR(device, swapchain, nullptr);
+		swapchain = nullptr;
+	}
+
+	if (depthImageView)
+	{
+		vkDestroyImageView(device, depthImageView, nullptr);
+		vmaDestroyImage(vmaAllocator, depthImage, depthImageAllocation);
+		depthImageView = nullptr;
+	}
 }
