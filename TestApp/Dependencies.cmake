b9fc101bc2b2e6594fb1cab9f6ebb643126aa0da
HackFight <alyx.cornu@gmail.com>
2026-08-18 03:03:13 +0200

    Added tinyGLTF

--- TestApp/Dependencies.cmake
+++ TestApp/Dependencies.cmake
@@ -1,14 +1,11 @@
 include(FetchContent)
 
-# shaderc comes from the Vulkan SDK component (Vulkan::shaderc_combined) on
-# both platforms. Windows: install the LunarG Vulkan SDK. Arch: `pacman -S
-# shaderc vulkan-headers vulkan-icd-loader`. Requires CMake >= 3.24 for the
-# shaderc_combined component to be recognized by FindVulkan.
 find_package(Vulkan REQUIRED COMPONENTS shaderc_combined)
 
 add_subdirectory(third_party/volk)
 add_subdirectory(third_party/vma)
 add_subdirectory(third_party/glm)
+add_subdirectory(third_party/tiny_gltf)
 
 set(SDL_SHARED OFF CACHE BOOL "" FORCE)
 set(SDL_STATIC ON CACHE BOOL "" FORCE)
